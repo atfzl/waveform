cdaa28b11d94e740556d05ccc70df3a1ead6e20c
Atif Afzal <atif.afzal@paytm.com>
2016-12-31 12:20:30 +0530

    Use tcomb only in development

--- build/index.js
+++ build/index.js
@@ -5,10 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = add;
 
-var _tcomb = require('tcomb');
-
-var _tcomb2 = _interopRequireDefault(_tcomb);
-
 var _data = require('./data');
 
 var _data2 = _interopRequireDefault(_data);
@@ -23,36 +19,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 // eslint-disable-next-line import/first
 function add(a, b) {
-  _assert(a, _tcomb2.default.Number, 'a');
-
-  _assert(b, _tcomb2.default.Number, 'b');
-
-  var ret = function (a, b) {
-    return a + b;
-  }.call(this, a, b);
-
-  _assert(ret, _tcomb2.default.Number, 'return value');
-
-  return ret;
+  return a + b;
 }
 
-console.log('' + add(_data2.default, 2));
-
-function _assert(x, type, name) {
-  if (false) {
-    _tcomb2.default.fail = function (message) {
-      console.warn(message);
-    };
-  }
-
-  if (_tcomb2.default.isType(type) && type.meta.kind !== 'struct') {
-    if (!type.is(x)) {
-      type(x, [name + ': ' + _tcomb2.default.getTypeName(type)]);
-    }
-  } else if (!(x instanceof type)) {
-    _tcomb2.default.fail('Invalid value ' + _tcomb2.default.stringify(x) + ' supplied to ' + name + ' (expected a ' + _tcomb2.default.getTypeName(type) + ')');
-  }
-
-  return x;
-}
+console.log('' + add(_data2.default, 3));
 //# sourceMappingURL=index.js.map
